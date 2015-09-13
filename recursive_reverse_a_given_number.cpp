5f172645357beb43399774abaf0637ee47235d54
Narendra Kumar <Narendra-JNU-2k13@users.noreply.github.com>
2015-09-14 01:43:47 +0530

    Update recursive_reverse_a_given_number.cpp

--- recursive_reverse_a_given_number.cpp
+++ recursive_reverse_a_given_number.cpp
@@ -1,34 +1,29 @@
 #include<stdio.h>
-
 int reverse_no_recursively(int n)
 {   static int result=0;
     if(n>0)
         { 
-	   result+=(n%10);
-	  
+	  result+=(n%10);
 	  if(n/10!=0)
-             result*=10;
-			  
+            result*=10;
 	   reverse_no_recursively(n/10);         
           }
-	     return result; 	
+    return result; 	
  }
 
-	int main()
-        {
-	  int input,output;
-	   printf("\n \t Enter a no : - ");
-	   scanf("%d",&input);
-	   printf("\n\t Reversed Digits of given no : - ");
-	   if(input<0)   // to handle negative number.
-	  {            // it will convert into positive no and 
-	               //prefixed the output by negative(-) sign
+ int main()
+   {
+     int input,output; 
+     printf("\n \t Enter a no : - ");
+     scanf("%d",&input); 
+     printf("\n\t Reversed Digits of given no : - ");
+     if(input<0)   // to handle negative number.
+	  {            // it will convert into positive no and prefixed the output by negative(-) sign.
 	      input=input*(-1);	       
-	       printf("-");							            }		
-	   
+	       printf("-");				
+	  }		
         output=reverse_no_recursively(input);
-	 									        printf("%d",output);
-											    getchar();
-									           return getchar();
-										}
+        printf("%d",output);
+        return getchar();
+  }
 
