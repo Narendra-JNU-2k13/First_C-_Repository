dca32055a323f06352f15038b0d446d670f18c28
Narendra Kumar <Narendra-JNU-2k13@users.noreply.github.com>
2015-09-14 02:29:34 +0530

    Update subset_sum_problem.cpp

--- subset_sum_problem.cpp
+++ subset_sum_problem.cpp
@@ -10,52 +10,53 @@ int flag; //to check output exist or not
 void print_output()
 {
    printf("\n\t");
-      for(int i=0;i<no_of_element;i++)
+   for(int i=0;i<no_of_element;i++)
+   {
+    if(chararr[i]=='1')
+    printf(" %d +",intarr[i]);        
+    }  
+   printf("\b= %d",sum);
+ }
+
+   void subset_sum(int n)
+   {
+     if(n<1)
+      {  
+      	int rsum=0;
+        for(int i=0;i<no_of_element;i++)
+         { 
+	      if(chararr[i]=='1')
+              rsum+=intarr[i];
+         } 
+        if(rsum==sum)
          {
-	       if(chararr[i]=='1')
-	                printf(" %d +",intarr[i]);        
-			   }  
-			      printf("\b= %d",sum);
-			      }
-
-			      void subset_sum(int n)
-			      {
-			          
-				    if(n<1)
-				      {      int rsum=0;
-				               for(int i=0;i<no_of_element;i++)
-					                { 
-							            if(chararr[i]=='1')
-								                rsum+=intarr[i];
-										         } 
-											          if(rsum==sum)
-												           { flag=1;
-													                print_output();
-															         }   
-																   }
-																     else
-																       {
-																             chararr[n-1]='0';
-																	           subset_sum(n-1);
-																		         chararr[n-1]='1';
-																			       subset_sum(n-1);
-																			         }       
-																				 }
-
-																				 int main()
-																				 {
-																				     int i;
-																				         cout<<"\n How many elements do u want in array : - ";
-																					     cin>>no_of_element;
-																					         for(i=0;i<no_of_element;i++)
-																						     cin>>intarr[i];
-																						         cout<<"\n Enter sum equivalent : - ";
-																							     cin>>sum;
-																							         flag=0;
-																								     subset_sum(no_of_element);
-																								         if(flag==0)
-																									     printf("\n\n\t Sorry No output  exist\n\n");
-																									         getchar();
-																										     return getchar();
-																										     }    
+           flag=1;
+           print_output();
+	  }
+      }
+      else
+      {
+      	 chararr[n-1]='0';
+      	 subset_sum(n-1);
+      	 chararr[n-1]='1';
+      	 subset_sum(n-1);
+       }
+    }
+    
+      int main()
+       {
+       	 int i;
+       	 cout<<"\n How many elements do u want in array : - ";
+       	 cin>>no_of_element;
+       	 for(i=0;i<no_of_element;i++)
+       	   cin>>intarr[i];
+       	 cout<<"\n Enter sum equivalent : - ";
+       	 cin>>sum;
+       	 flag=0;
+       	 subset_sum(no_of_element);
+       	 if(flag==0)
+       	 printf("\n\n\t Sorry No output  exist\n\n");
+       	 getchar();
+       	 return getchar();
+       }    
 
