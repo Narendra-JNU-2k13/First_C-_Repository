81de27a76aafda36504362aa39358b557a716a9c
naren <sdraj.narendra@gmail.com>
2015-09-14 01:34:22 +0530

    C++ program file

---
+++ recursive_reverse_a_given_number.cpp
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+int reverse_no_recursively(int n)
+{   static int result=0;
+    if(n>0)
+        { 
+	   result+=(n%10);
+	  
+	  if(n/10!=0)
+             result*=10;
+			  
+	   reverse_no_recursively(n/10);         
+          }
+	     return result; 	
+ }
+
+	int main()
+        {
+	  int input,output;
+	   printf("\n \t Enter a no : - ");
+	   scanf("%d",&input);
+	   printf("\n\t Reversed Digits of given no : - ");
+	   if(input<0)   // to handle negative number.
+	  {            // it will convert into positive no and 
+	               //prefixed the output by negative(-) sign
+	      input=input*(-1);	       
+	       printf("-");							            }		
+	   
+        output=reverse_no_recursively(input);
+	 									        printf("%d",output);
+											    getchar();
+									           return getchar();
+										}
+
